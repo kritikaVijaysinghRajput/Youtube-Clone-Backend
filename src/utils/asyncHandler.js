08e9debe6c0ad28887ee8a0ecf5ef3b1442baa2c
kritikaVijaysinghRajput <kritikarajput203@gmail.com>
2024-07-20 19:10:05 +0530

    Upto Video and User Model done

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+//By PROMISE method
+const asyncHandler = () => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+//By TRY CATCH method
+// const asyncHandler = (fn) =>async (req,res,next){
+//     try {
+//         await fn(req, res, next)
+
+//     } catch (error) {
+//         res.status(err.code ||500).jso({
+//             success:false,
+//             message:err.message
+//         })
+
+//     }
+// }
